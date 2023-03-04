fc5369f303b0a5cc940d8edbf244a078c92e2585
dax <125095504+daxyys@users.noreply.github.com>
2023-03-04 14:28:54 -0500

    ui libs fr
    fr

---
+++ atlas.lua
@@ -0,0 +1,16 @@
+local Atlas = loadstring(game:HttpGet("https://siegehub.net/Atlas.lua"))()
+
+local UI = Atlas.new({
+    Name = "My Script"; -- script name (required)
+    ConfigFolder = "MyScript"; -- folder name to save configs to, set this to nil if you want to disable configs saving
+    Credit = "Made By RoadToGlory#9879"; -- text to appear if player presses the "i" button on their UI (optional)
+    Color = Color3.fromRGB(255,0,0); -- theme color for UI (required)
+    Bind = "LeftControl"; -- keybind for toggling the UI, must be a valid KeyCode name
+    -- Atlas Loader:
+    UseLoader = true; -- whether to use Atlas Loader or not, if set to false than CheckKey and Discord are ignored
+    FullName = "My Script Hub"; -- works if UseLoader is set to true, can be same as Name argument, shown on loader frame
+    CheckKey = function(inputtedKey) -- works if UseLoader is set to true, where inputtedKey is the key inputted by the player
+        return inputtedKey=="key1234"
+    end;
+    Discord = "https://discord.gg/xu5dDS3Pb9"; -- works if UseLoader is set to true, will be copied to clipboard if player presses "Copy Invite" button
+})
